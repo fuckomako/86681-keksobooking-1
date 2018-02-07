049c9e799c212201363c378167053f868081e1b0
Dmitry Malnev <dmitry.malnev@mkrf.ru>
2018-02-07 15:37:10 +0300

    Избавился от вложенности + Исправил вывод ошибки

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const args = process.argv.slice(2);
 
+if (args.length === 0) {
+  console.log('Привет пользователь!\n\r' +
+    'Эта программа будет запускать сервер «keksobooking».\n\r'+
+    'Автор: Кекс.');
+    process.exit(0);
+};
+
 switch (args[0]) {
   case '--version':
     console.log('v0.0.1');
+    process.exit(0);
     break;
 
   case '--help':
     console.log('Доступные команды:\n\r' +
     '--help    — печатает этот текст;\n\r' +
     '--version — печатает версию приложения;');
+    process.exit(0);
     break;
 
   default:
-    if (args.length === 0) {
-      console.log('Привет пользователь!\n\r' +
-        'Эта программа будет запускать сервер «keksobooking».\n\r'+
-        'Автор: Кекс.');
-    } else {
-      args.forEach(arg =>
-        console.error(`Неизвестная команда ${arg}.`)
-      );
-      console.error('Чтобы прочитать правила использования приложения, наберите "--help"');
-      process.exit(1);
-    };
-}
+    console.error(`Неизвестная команда ${args[0]}.`);
+    console.error('Чтобы прочитать правила использования приложения, наберите "--help"');
+    process.exit(1);
+};
