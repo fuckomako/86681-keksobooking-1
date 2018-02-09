4b62477bbc5ce8c806243b0bc74bfd60be5bfe83
Dmitry Malnev <dmitry.malnev@mkrf.ru>
2018-02-09 11:42:12 +0300

    Настроил проект

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log('Привет пользователь!\n\r' +
-    'Эта программа будет запускать сервер «keksobooking».\n\r'+
-    'Автор: Кекс.');
-    process.exit(0);
-};
+  console.log(`Привет пользователь!\n\r` +
+    `Эта программа будет запускать сервер «keksobooking».\n\r` +
+    `Автор: Кекс.`);
+  process.exit(0);
+}
 
 switch (args[0]) {
-  case '--version':
-    console.log('v0.0.1');
+  case `--version`:
+    console.log(`v0.0.1`);
     break;
 
-  case '--help':
-    console.log('Доступные команды:\n\r' +
-    '--help    — печатает этот текст;\n\r' +
-    '--version — печатает версию приложения;');
+  case `--help`:
+    console.log(`Доступные команды:\n\r` +
+    `--help    — печатает этот текст;\n\r` +
+    `--version — печатает версию приложения;`);
     break;
 
   default:
     console.error(`Неизвестная команда ${args[0]}.`);
-    console.error('Чтобы прочитать правила использования приложения, наберите "--help"');
+    console.error(`Чтобы прочитать правила использования приложения, наберите "--help"`);
     process.exit(1);
-};
+}
