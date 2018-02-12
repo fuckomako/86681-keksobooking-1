4010a47eabc9fe5b75fa3312dbcba5ecefca0fa3
Dmitry Malnev <dmitry.malnev@mkrf.ru>
2018-02-12 11:34:11 +0300

    по фиксил баги

--- index.js
+++ index.js
@@ -12,19 +12,3 @@ const commands = [version, help, author, license, description];
 const currentCommand = commands.find((command)=>`--${command.name}` === args[0]) || start;
 
 currentCommand.execute(args[0]);
-
-
-// const start = require(`./src/default`);
-// const version = require(`./src/version`);
-// const help = require(`./src/help`);
-// const author = require(`./src/author`);
-// const license = require(`./src/license`);
-// const description = require(`./src/description`);
-//
-// const args = process.argv.slice(2);
-//
-// const commands = [version, help, author, license, description];
-//
-// const currentCommand = commands.find((command)=>`--${command.name}` === args[0]) || start;
-//
-// currentCommand.execute(args[0]);
--- src/author.js
+++ src/author.js
@@ -4,6 +4,6 @@ module.exports = {
   name: `author`,
   description: `Show programm author`,
   execute() {
-    console.log(`автор: ${packageInfo}`);
+    console.log(`автор: ${packageInfo.author}`);
   }
 };
