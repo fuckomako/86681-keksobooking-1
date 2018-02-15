dad6aca3743a073430601efbe74279a18b406199
fuckomako <shnoop87@yandex.ru>
2018-02-15 22:43:20 +0300

    Разукрашка

--- src/default.js
+++ src/default.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-require(`colors`);
-
-module.exports = {
-  name: `default`,
-  description: `Shows default and errors`,
-  execute(arg) {
-    if (arg) {
-      console.error(`Неизвестная команда ${arg}.
-Чтобы прочитать правила использования приложения, наберите "--help"`.cyan);
-      process.exit(1);
-    }
-    console.log(`Привет пользователь!
-Эта программа будет запускать сервер «Keksobooking».
-Автор: Кекс.`.cyan);
-=======
 module.exports = {
   name: `default`,
   description: `Начало программы и ошибки`,
@@ -27,6 +11,5 @@ module.exports = {
 Эта программа будет запускать сервер «Keksobooking».
 Автор: Кекс.`);
     return true;
->>>>>>> origin/module2-task2
   }
 };
--- src/help.js
+++ src/help.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-require(`colors`);
-const version = require(`./version`);
-const author = require(`./author`);
-const license = require(`./license`);
-const description = require(`./description`);
-const otherCommands = [version, author, license, description];
-
-module.exports = {
-  name: `help`,
-  description: `Shows help`,
-  execute() {
-    console.log(`Это приложение ничего не делает. Доступные команды:
-    \n${[this, ...otherCommands]
-      .map((command) => `--${command.name.grey}:\t\t-${command.description.green}`)
-      .join(`\n`)}
-    `);
-  },
-=======
+const colors = require(`colors/safe`);
 const commonCommands = require(`./commands/common-commands`);
 
 module.exports = {
@@ -25,7 +7,6 @@ module.exports = {
   execute() {
     const commands = [this, ...commonCommands];
     console.log(`Доступные команды:
-${commands.map((it) => `--${it.name.padEnd(12)} ${it.description}`).join(`\n`)}`);
+${commands.map((it) => `--${colors.grey(it.name.padEnd(12))} ${colors.green(it.description)}`).join(`\n`)}`);
   }
->>>>>>> origin/module2-task2
 };
